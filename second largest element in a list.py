2ed556a515cfcdab3edc476b26cebdf38f6f767e
6saicharan66 <69040144+6saicharan66@users.noreply.github.com>
2020-10-07 21:28:49 +0530

    Update second largest element in a list.py

--- second largest element in a list.py
+++ second largest element in a list.py
@@ -1,11 +1,18 @@
-#python 3.7.1
-
 list =[]
+
 num = int(input("enter no.of elements:"))
-for i in range(1, num+1):
+
+for i in rlistange(1, num+1):
+
   elements=int(input("enter elements:"))
+
   list.append(elements)
+
   
+
 list.sort()
+
   
+
 print("largest element is:", sorted(list)[-2])
+
